87a99493a630d7cbc7f5fc4f4519c7c3e9d2cf18
michaelhesse <michaelhesse12345@gmail.com>
2026-09-25 11:29:57 +0000

    Updated luxury UI templates and production configuration

--- nailsalon/settings.py
+++ nailsalon/settings.py
@@ -133,8 +133,11 @@ USE_TZ = True
 # Static files configuration
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+# Tells Django where to look for app-level static files
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'bbnails', 'static'),
+]
 MEDIA_URL = '/media/'
 
 
